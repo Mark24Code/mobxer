527516c5b02bdc880b076ae0d7059ac04a63763a
Mark24Code <mark.zhangyoung@gmail.com>
2018-07-22 20:14:55 +0800

    feat(scroll): aminatedScrollTop

--- src/App.js
+++ src/App.js
@@ -3,6 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    count : 10000,
+  }
+  banners = () => {
+    const banners = [];
+    const {count} = this.state;
+    for(let i=0; i< count; i++){
+      banners.push(<h1 className="App-title" key={i}>Welcome to React-  {i} </h1>)
+    }
+    return banners
+  }
+  
+  // targetHeight= 0;
+  animatedScrollTop = (targetHeight) => {
+    targetHeight = targetHeight || 0;
+    function animatedScroll(){
+      const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+      if (currentScroll > targetHeight) {
+        window.scrollTo(0, currentScroll - currentScroll / 5);
+        window.requestAnimationFrame(animatedScroll);
+      }
+    }
+    window.requestAnimationFrame(animatedScroll);
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +35,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        {
+          this.banners()
+        }
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get startstamped, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="float-btn" onClick={this.animatedScrollTop.bind(this,1000)}>TOP</div>
       </div>
     );
   }
